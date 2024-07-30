a886b631abeab6eee26b1335c7bede25847bac53
JuditAF <idryscreep@gmail.com>
2024-07-30 13:25:47 +0200

    guardo la landing en routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 // IMPORTACION PAGINAS
 import { RegistroComponent } from './pages/registro/registro.component';
 import { LoginComponent } from './pages/login/login.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 const routes: Routes = [
   { path: 'register', component: RegistroComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'landing', component: LandingComponent}
 ];
 
 @NgModule({

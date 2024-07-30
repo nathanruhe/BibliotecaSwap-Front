ed5ae385d6cbfbccda824ec1e6a7e536560e2489
JuditAF <idryscreep@gmail.com>
2024-07-30 23:00:30 +0200

    subo las rutas a app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { LoginComponent } from './pages/login/login.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { AnadirLibroComponent } from './pages/anadir-libro/anadir-libro.component';
+import { EditarLibroComponent } from './pages/editar-libro/editar-libro.component';
 
 const routes: Routes = [
   { path: 'register', component: RegistroComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'landing', component: LandingComponent}
+  { path: 'landing', component: LandingComponent },
+  { path: 'addLibro', component: AnadirLibroComponent },
+  { path: 'editLibro', component: EditarLibroComponent },
 ];
 
 @NgModule({

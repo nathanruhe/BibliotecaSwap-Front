9c0522b6e08ceace4f77d7e2d77faf259485a014
nathanruhe <nathan.ruhe@hotmail.com>
2024-08-01 22:09:51 +0200

    subida pagina solicitud / ajustes pagina perfil-otros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,14 @@ import { RegistroComponent } from './pages/registro/registro.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PerfilOtrosComponent } from './pages//perfil-otros/perfil-otros.component';
-import { BibliotecaComponent } from './pages/biblioteca/biblioteca.component';
+import { SolicitudComponent } from './pages/solicitud/solicitud.component';
 
 const routes: Routes = [
   { path: 'register', component: RegistroComponent },
   { path: 'login', component: LoginComponent },
   {path: 'home', component: HomeComponent},
   { path: 'perfil-otros', component: PerfilOtrosComponent },
-  {path: 'miBibliote', component: BibliotecaComponent},
+  { path: 'solicitud', component: SolicitudComponent },
 ];
 
 @NgModule({

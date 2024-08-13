185b855834cb87d1011fa08f8dd1c24a650898b9
OddQueen <queensleymei@gmail.com>
2024-08-14 01:13:36 +0200

    model chat

---
+++ src/app/pages/solicitud/solicitud.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitud',
+  templateUrl: './solicitud.component.html',
+  styleUrls: ['./solicitud.component.css']
+})
+export class SolicitudComponent {
+
+}

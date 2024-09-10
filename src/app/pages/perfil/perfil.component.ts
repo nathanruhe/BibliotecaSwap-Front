86181a6ff8e4cfea222084b34600fa68828af396
JuditAF <idryscreep@gmail.com>
2024-09-10 14:04:29 +0200

    arreglada página perfil

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -37,17 +37,19 @@ export class PerfilComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const userStorage = localStorage.getItem('user');
-    this.user = JSON.parse(userStorage);
+    const userId = +localStorage.getItem('userId');
+    this.user = this.userService.user;
     
-    this.userService.profile(this.user.id_user).subscribe((response: any) => {
+    this.userService.profile(userId).subscribe((response: any) => {
+      console.log(response.dataUser);
+      
+      this.userService.user = response.dataUser.user;
+      this.user = this.userService.user;
       this.rating = response.dataUser.rating;
       this.misResenas = response.dataUser.misResenas;
       // this.genreIcons = response.dataUser.user.genres;
-      console.log(this.user.id_user);
+      console.log(userId);
       console.log(this.user.genres);
-      
-      
     })
 
   }
